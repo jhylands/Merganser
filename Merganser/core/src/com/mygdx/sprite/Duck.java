c7cf12a59894f6de474da34715aba3e982ecf123
= <=>
2015-11-24 10:54:21 +0000

    Added duck rotation functionality

--- Merganser/core/src/com/mygdx/sprite/Duck.java
+++ Merganser/core/src/com/mygdx/sprite/Duck.java
@@ -11,15 +11,18 @@ public class Duck {
 	private int screenWidth = Gdx.graphics.getWidth();
 	private int duckSpeed = 2;
 	
-	private Texture duck = new Texture("large_duck.png");
-	private Vector2 duckLocation = new Vector2(20, (screenHeight / 2) - (duck.getHeight() / 2));
-
+	
+	
+	
+	private Texture[] duck = {new Texture("large_duck.png"),new Texture("large_duck_down.png"),new Texture("large_duck_left.png"),new Texture("large_duck_right.png")};
+	private Vector2 duckLocation = new Vector2(20, (screenHeight / 2) - (duck[0].getHeight() / 2));
+	private int Rotation = 0;
 	public Vector2 getDuckLocation(){
 		return duckLocation;
 	}
 	
 	public Texture getTexture(){
-		return duck;
+		return duck[this.Rotation];
 	}
 
 	public int getDuckSpeed() {
@@ -35,28 +38,26 @@ public class Duck {
 	}
 	
 	public int getWidth(){
-		return duck.getWidth();
+		return duck[this.Rotation].getWidth();
 	}
 	
 	public int getHeight(){
-		return duck.getHeight();
+		return duck[this.Rotation].getHeight();
 	}
 	
 	public void getDuckMovement() {
 		if (Gdx.input.isKeyPressed(Keys.W) && (duckLocation.y < (screenHeight - getHeight()))) {
 			duckLocation.y += duckSpeed;
-		}
-
-		if (Gdx.input.isKeyPressed(Keys.S) && (duckLocation.y > 0)) {
+			this.Rotation = 0;
+		}else if (Gdx.input.isKeyPressed(Keys.S) && (duckLocation.y > 0)) {
 			duckLocation.y -= duckSpeed;
-		}
-
-		if (Gdx.input.isKeyPressed(Keys.A) && (duckLocation.x > 0)) {
+			this.Rotation = 1;
+		}else if (Gdx.input.isKeyPressed(Keys.A) && (duckLocation.x > 0)) {
 			duckLocation.x -= duckSpeed;
-		}
-
-		if (Gdx.input.isKeyPressed(Keys.D) && (duckLocation.x < (screenWidth - duck.getWidth()))) {
+			this.Rotation=2;
+		}else if (Gdx.input.isKeyPressed(Keys.D) && (duckLocation.x < (screenWidth - duck[3].getWidth()))) {
 			duckLocation.x += duckSpeed;
+			this.Rotation=3;
 		}
 	}
 	
